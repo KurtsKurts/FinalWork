787155fa06e5bc0feb80e47d13fffc37cd0ca93a
“KurtsKurts” <Alex23.89@mail.ru>
2023-01-27 15:38:36 +0700

    Program written

--- Program.cs
+++ Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿string[] array = { "hello", "2", "world", ":-)", "1234", "123", "-2" };
+
+
+string[] ThreeSymbolsFilterArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] newArray = new string[count];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
+
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+}
+
+
+PrintArray(ThreeSymbolsFilterArray(array));
